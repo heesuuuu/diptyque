ef866e0c651ce50c7a842aaf72d17056d042d906
2mighytMt <kangsan5262@gmail.com>
2025-03-06 19:11:27 +0900

    [feat] pr rule test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
     return (
         <Router>
-            <Routes></Routes>
+            <Routes>gdgdg</Routes>
         </Router>
     );
 }
