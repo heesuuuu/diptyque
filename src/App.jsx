ebd155d834fbd7704c4e5e0b01ee736e8646ef8d
rabbit-onion <csr1000@naver.com>
2025-03-09 19:27:37 +0900

    [setting] 코드 컨벤션 유지용 json파일 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './common/Layout';
 import './styles/globals.scss';
-import { Cart, Collection, GuestOrder, Maison, Member, Mypage, Payment, Product, Promotion, SearchResult, Service } from './pages';
+import {
+  Cart,
+  Collection,
+  GuestOrder,
+  Maison,
+  Member,
+  Mypage,
+  Payment,
+  Product,
+  Promotion,
+  SearchResult,
+  Service,
+} from './pages';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Maison />} />
-          <Route path='/promotion' element={<Promotion />} />
-          <Route path='/product' element={<Product />} />
-          <Route path='/collection' element={<Collection />} />
-          <Route path='/service' element={<Service />} />
-          <Route path='/member' element={<Member />} />
-          <Route path='/mypage' element={<Mypage />} />
-          <Route path='/payment' element={<Payment />} />
-          <Route path='/guestorder' element={<GuestOrder />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/searchresult' element={<SearchResult />} />
+          <Route path="/promotion" element={<Promotion />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/member" element={<Member />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/guestorder" element={<GuestOrder />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/searchresult" element={<SearchResult />} />
         </Route>
       </Routes>
     </Router>
