820bffb877e88d6562b9356292c3d0f0ebbcd79e
lse-7660 <lse76609251@gmail.com>
2025-03-10 13:03:36 +0900

    [feat] 장바구니 기능 진행 중

---
+++ src/components/cart/CartItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CartItem = () => {
+  return (
+    <div className="cart-item">
+      <img src="" alt="" />
+      <p></p>
+    </div>
+  );
+};
+
+export default CartItem;
