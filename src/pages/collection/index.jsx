e1400d1a4e8bdd7c5abd0bc909e28a08f65faa40
정희수 <gmltn3780@naver.com>
2025-03-11 16:04:19 +0900

    [chore,style] 컬렉션 페이지 레이아웃, 데이터 불러오는 중

--- src/pages/collection/index.jsx
+++ src/pages/collection/index.jsx
@@ -1,5 +1,30 @@
+import './styles.scss';
 const Collection = () => {
-  return <div></div>;
+  return (
+    <>
+      {/* Collection 배너이미지 Sec */}
+
+      <section className="mb-[200px]">
+        <img src="" alt="" className="bg-gray-400 w-full h-[841px]" />
+      </section>
+
+      {/* Collection 소개 Sec */}
+      <div className='px-[80px]'>
+        <section className="mt-[80px] mb-[200px] w-[1168px] mx-auto">
+          {/* 소개 Title */}
+          <div className="font-diptyque text-heading1 text-center">Philosykos</div>
+          <div className="mt-[40px]">
+            Body Medium. Most fonts have a particular weight which corresponds to one of the numbers in Common weight
+            name mapping. However some fonts, called variable fonts, can support a range of weights with a more or less
+            fine granularity, and this can give the designer a much closer degree of control over the chosen weight.
+          </div>
+        </section>
+
+        {/* Collection 상품 Sec */}
+        <section>dd</section>
+      </div>
+    </>
+  );
 };
 
 export default Collection;
--- src/store/modules/collectionSlice.js
+++ src/store/modules/collectionSlice.js
@@ -1,12 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
+import perfumeData from '../../data/perfume_updated.json';
+import diffuserData from '../../data/diffuser_updated.json';
+import candleData from '../../data/candle_updated.json';
+import bodyData from '../../data/body_updated.json';
 
-const initialState = {
-    
+const getItemsColName = (dataArray) => {
+  return dataArray.fiter((item) => item.collection !== null && item.collection !== undefined);
 };
+
+const perfumeColName = getItemsColName(perfumeData);
+const diffuserColName = getItemsColName(diffuserData);
+const candleColName = getItemsColName(candleData);
+const bodyColName = getItemsColName(bodyData);
+
+const allCollections = [...perfumeColName, ...diffuserColName, ...candleColName, ...bodyColName];
+
+const initialState = {};
 export const collectionSlice = createSlice({
-    name: 'collection',
-    initialState,
-    reducers: {},
+  name: 'collection',
+  initialState,
+  reducers: {},
+
+  extraReducers: (builder) => {},
 });
 export const collectionActions = collectionSlice.actions;
 export default collectionSlice.reducer;
