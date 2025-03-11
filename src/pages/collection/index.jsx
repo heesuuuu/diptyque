1db76974785198b33ca759cc3c9c54ec9357c9a5
정희수 <gmltn3780@naver.com>
2025-03-11 17:46:35 +0900

    [chore] 컬렉션 수정중

--- src/pages/collection/index.jsx
+++ src/pages/collection/index.jsx
@@ -1,4 +1,4 @@
-import './styles.scss';
+
 const Collection = () => {
   return (
     <>
--- src/store/modules/collectionSlice.js
+++ src/store/modules/collectionSlice.js
@@ -4,22 +4,35 @@ import diffuserData from '../../data/diffuser_updated.json';
 import candleData from '../../data/candle_updated.json';
 import bodyData from '../../data/body_updated.json';
 
-const getItemsColName = (dataArray) => {
-  return dataArray.fiter((item) => item.collection !== null && item.collection !== undefined);
-};
+// const getItemsColName = (dataArray) => {
+//   return dataArray.fiter((item) => item.collection !== null && item.collection !== undefined);
+// };
 
-const perfumeColName = getItemsColName(perfumeData);
-const diffuserColName = getItemsColName(diffuserData);
-const candleColName = getItemsColName(candleData);
-const bodyColName = getItemsColName(bodyData);
+// const perfumeColName = getItemsColName(perfumeData);
+// const diffuserColName = getItemsColName(diffuserData);
+// const candleColName = getItemsColName(candleData);
+// const bodyColName = getItemsColName(bodyData);
 
-const allCollections = [...perfumeColName, ...diffuserColName, ...candleColName, ...bodyColName];
+// 모든 컬렉션 이름 합침
+// const allCollections = [...perfumeColName, ...diffuserColName, ...candleColName, ...bodyColName];
+
+// // 컬렉션 이름 배열로 생성
+// const AllCollectionNames = [...new Set(allCollections.map((item) => item.collection))];
+
+// const collectionGroups = {};
+// allCollections.forEach((collectionName) => {
+//   collectionGroups[collectionName] = allCollections.filter((item) => item.collection === collectionName);
+// });
 
 const initialState = {};
 export const collectionSlice = createSlice({
+//   getItemsColName,
+//   AllCollectionNames,
   name: 'collection',
   initialState,
-  reducers: {},
+  reducers: {
+    update: (state, action) => {},
+  },
 
   extraReducers: (builder) => {},
 });
