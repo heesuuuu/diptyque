9ce90d8cdb357c0a8d507e7965acda71aabb7ae9
rabbit-onion <csr1000@naver.com>
2025-03-12 10:56:57 +0900

    [chore] 동기화 전 정리

--- src/pages/productDetail/index.jsx
+++ src/pages/productDetail/index.jsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { productActions } from '../../store/modules/productSlice';
+import BarButton from '../../ui/BarButton';
+import Icon from '../../ui/Icon';
 
 const ProductDetail = () => {
   const dispatch = useDispatch();
@@ -23,31 +25,46 @@ const ProductDetail = () => {
 
   return (
     <>
-      <div>
-        {/* <img src={options[0].images.thumbnail.default} alt={name} />
-        <img src={options[0].images.thumbnail.hover} alt={name} /> */}
-        {options[0].images.detail.map((img, idx) => (
-          <img key={idx} src={img} alt={name} />
-        ))}
-      </div>
-      <div>
-        <h1>{name}</h1>
-        <p>
-          {type}
-          <span>
-            {options[0].size} ml | €{options[0].price}
-          </span>
-        </p>
-        <hr />
-        <p>{notes.map((note, idx) => (idx !== notes.length - 1 ? note + ', ' : note))}</p>
-        <p>{description}</p>
-        <div>
-          <label htmlFor="selectOption">Add Personalization</label>
-          <select name="selectOption" id="selectOption">
-            <option value="">select</option>
-            <option value=""></option>
-            <option value=""></option>
-          </select>
+      <div className="flex">
+        <div className="w-1/2">
+          {options[0].images.detail.map((img, idx) => (
+            <img key={idx} src={img} alt={name} className="w-full h-auto" />
+          ))}
+        </div>
+        <div className="fixed top-0 right-0 w-1/2">
+          <h1>{name}</h1>
+          <p>
+            {type}
+            <span>
+              {options[0].size} ml | €{options[0].price}
+            </span>
+          </p>
+          <hr />
+          <p>{notes.map((note, idx) => (idx !== notes.length - 1 ? note + ', ' : note))}</p>
+          <p>{description}</p>
+          <div>
+            <label htmlFor="selectOption">Add Personalization</label>
+            <select name="selectOption" id="selectOption">
+              <option value="">select</option>
+              <option value=""></option>
+              <option value=""></option>
+            </select>
+          </div>
+          <div>
+            <p>
+              Story
+              <Icon name="keyboard_arrow_down" />
+            </p>
+            <p>
+              Ingredients
+              <Icon name="keyboard_arrow_down" />
+            </p>
+          </div>
+          <BarButton type="filled" text="ADD TO BAG" />
+          <div className="flex justify-center items-center bg-black text-white">
+            <Icon name="keyboard_arrow_down" />
+            ADDED TO BAG
+          </div>
         </div>
       </div>
     </>
