6e130eedd68f05536d7549182908567917d3543d
정희수 <gmltn3780@naver.com>
2025-03-13 00:09:52 +0900

    [chore,style] 데이터 collectionName 수정, 상품 사진 배치중

--- src/components/collection/CollectionMenu.jsx
+++ src/components/collection/CollectionMenu.jsx
@@ -1,22 +1,29 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+// components/collection/CollectionMenu.jsx
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectCollection } from '../../store/modules/collectionSlice';
 
 const CollectionMenu = () => {
-  const { allCollectionNames } = useSelector((state) => state.collection);
+  const dispatch = useDispatch();
+  const { allCollectionNames, selectedCollection } = useSelector((state) => state.collection);
 
+  const handleCollectionClick = (collection) => {
+    dispatch(selectCollection(collection));
+  };
 
   return (
     <div className="collection-menu">
       <ul className="space-y-[10px]">
         {allCollectionNames.map((collection, index) => (
           <li key={index}>
-            <Link
-              to={`/collection/${encodeURIComponent(collection)}`}
-              className="font-diptyque text-heading2 transition-colors"
+            <div
+              onClick={() => handleCollectionClick(collection)}
+              className={`font-diptyque text-heading2 transition-colors cursor-pointer ${
+                selectedCollection === collection ? 'text-primary' : ''
+              }`}
             >
               {collection}
-            </Link>
+            </div>
           </li>
         ))}
       </ul>
