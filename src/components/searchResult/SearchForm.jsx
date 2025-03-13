60fe62a305cb70577773605eeb11dda31009ab1f
lse-7660 <lse76609251@gmail.com>
2025-03-13 16:22:49 +0900

    [feat] 검색 페이지 진행 중

--- src/components/searchResult/SearchForm.jsx
+++ src/components/searchResult/SearchForm.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { useState } from 'react';
+import Icon from '../../ui/Icon';
 
 const SearchForm = () => {
-  return <div></div>;
+  const [text, setText] = useState('');
+
+  return (
+    <form className=" relative my-[280px] w-[668px] h-[44px] mx-auto border border-darkgrey-3">
+      <input type="text" value={text} className="" />
+      <button type="submit" className="absolute top-0 right-0 w-[44px] h-[44px]">
+        <Icon name="search" />
+      </button>
+    </form>
+  );
 };
 
 export default SearchForm;
--- src/pages/searchResult/index.jsx
+++ src/pages/searchResult/index.jsx
@@ -3,7 +3,7 @@ import SearchList from '../../components/searchResult/SearchList';
 
 const SearchResult = () => {
   return (
-    <div>
+    <div className="max-w-[1360px] mx-auto">
       <SearchForm />
       <SearchList />
     </div>
