bfd7c14cd11bd086eb14475a4d415715a174423b
정희수 <gmltn3780@naver.com>
2025-03-17 01:18:31 +0900

    [chore,style]popular 상품 추가중

--- src/components/collection/PopularProduct.jsx
+++ src/components/collection/PopularProduct.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
+import React, { useMemo } from 'react';
+import { BarButton } from '../../ui';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const PopularProduct = () => {
+  const { allProducts } = useSelector((state) => state.collection);
+
+  const topProducts = useMemo(() => {
+    if (!allProducts || allProducts.length === 0) {
+      return [];
+    }
+
+    return [...allProducts]
+      .filter(
+        (product) =>
+          product.sales !== undefined &&
+          product.options &&
+          product.options.length > 0 &&
+          product.options[0].images?.thumbnail?.default
+      )
+      .sort((a, b) => (b.sales || 0) - (a.sales || 0))
+      .slice(0, 5);
+  }, [allProducts]);
+
   return (
-    <div className="h-[519px] text-center ">
-      <div>Our most popular products</div>
-      <div>content</div>
+    <div className="text-center">
+      <div className="text-heading1 font-diptyque mb-10">Our most popular products</div>
+
+      <div className="overflow-x-auto max-w-full">
+        <div className="flex flex-nowrap space-x-6 px-4 pb-4">
+          {topProducts.map((product) => (
+            <div key={product.id} className="flex-none w-[423px]">
+              <Link to={`/product/${product.category}/${product.id}`}>
+                <div className="h-[504px] w-full bg-slate-100 overflow-hidden">
+                  {product.options[0].images?.thumbnail?.default && (
+                    <img
+                      src={product.options[0].images.thumbnail.default}
+                      alt={product.name}
+                      className="w-full h-full object-contain"
+                    />
+                  )}
+                </div>
+              </Link>
+              <div className="text-heading3 font-diptyque text-darkgrey-3 w-full text-left mt-4">{product.name}</div>
+              <div className="flex place-content-between mt-[10px]">
+                <div className="text-body3 text-darkgray-3">{product.type}</div>
+                <div className="flex text-body3 text-darkgrey-3">
+                  <div>{product.options[0].size}</div>
+                  <div className="mx-1">|</div>
+                  <div>{product.options[0].price} €</div>
+                </div>
+              </div>
+              <div className="mt-5">
+                <BarButton text="ADD TO BAG" type="filled" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default PopularProduct
+export default PopularProduct;
--- src/pages/collection/index.jsx
+++ src/pages/collection/index.jsx
@@ -1,49 +1,64 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import CollectionMenu from '../../components/collection/CollectionMenu';
-import CollectionProducts from '../../components/collection/CollectionProducts'; // 대소문자 수정
+import CollectionProducts from '../../components/collection/CollectionProducts';
 import PopularProduct from '../../components/collection/PopularProduct';
 
 const Collection = () => {
   const dispatch = useDispatch();
   const { selectedCollection, selectedCollectionDescription } = useSelector((state) => state.collection);
 
-  // 마우스가 메뉴 위에 있는지 상태
+  // Mouse over menu state
   const [isMouseOverMenu, setIsMouseOverMenu] = useState(false);
+  const [isFading, setIsFading] = useState(false);
 
-  // 메뉴 컨테이너 참조
   const menuContainerRef = useRef(null);
+  const descriptionRef = useRef(null);
 
-  // 컬렉션 변경 핸들러
   const handleCollectionChange = (collectionName) => {
-    console.log(`Collection changed to: ${collectionName}`);
-    // 추가 로직이 필요하다면 여기에 구현
+    setIsFading(true);
+
+    setTimeout(() => {
+      setIsFading(false);
+    }, 500);
   };
+
+  // 스크롤 위치를 디버깅하는 함수 (나중에 제거 가능)
+  const logScrollPosition = () => {
+    console.log(`Current scroll position: ${window.scrollY}px`);
+  };
+
+  useEffect(() => {
+  }, []);
 
   return (
     <>
-      {/* Collection 배너이미지 Sec */}
+      {/* 상단 배너 */}
       <section className="mb-[200px]">
-        {/* 빈 문자열 대신 null 또는 기본 이미지 경로 사용 */}
         <div className="bg-gray-400 w-full h-[841px]"></div>
       </section>
 
-      {/* Collection 소개 Sec */}
+      {/* Collection 소개 */}
       <div className="w-full px-[80px]">
-        <section className="mt-[80px] mb-[200px] lg:w-[1168px] lg:mx-auto">
-          {/* 소개 Title - 선택된 컬렉션 이름 표시 */}
+        <section
+          className={`mt-[80px] mb-[200px] lg:w-[1168px] lg:mx-auto transition-opacity duration-500 ${
+            isFading ? 'opacity-50' : 'opacity-100'
+          }`}
+        >
+          {/* 상단 title*/}
           <div className="font-diptyque text-heading1 text-center">{selectedCollection || 'Philosykos'}</div>
-          {/* 소개 설명 - 선택된 컬렉션 설명 표시 */}
-          <div className="mt-[40px] text-center">
+
+          {/* Introduction description*/}
+          <div ref={descriptionRef} className="mt-[40px] text-center transition-all duration-500">
             {selectedCollectionDescription ||
               'Body Medium. Most fonts have a particular weight which corresponds to one of the numbers in Common weight name mapping. However some fonts, called variable fonts, can support a range of weights with a more or less fine granularity, and this can give the designer a much closer degree of control over the chosen weight.'}
           </div>
         </section>
 
-        {/* Collection 상품 Sec */}
+        {/* Collection 상품*/}
         <section className="lg:w-full lg:mx-auto relative">
           <div className="flex flex-col md:flex-row">
-            {/* 왼쪽 메뉴 */}
+            {/* Left menu */}
             <div
               className="md:w-1/4 mb-8 md:mb-0 sticky top-[10vh] h-fit"
               ref={menuContainerRef}
@@ -53,15 +68,15 @@ const Collection = () => {
               <CollectionMenu />
             </div>
 
-            {/* 오른쪽 상품 목록/상세 정보 */}
+            {/* 상품 상세 */}
             <div className="md:w-3/4 lg:ml-[115px] md:ml-[24px]">
               <CollectionProducts onChangeCollection={handleCollectionChange} isMouseOverMenu={isMouseOverMenu} />
             </div>
           </div>
         </section>
 
-        {/* most popular Sec */}
-        <section className="mt-[80px] mb-[200px] lg:w-full lg:mx-auto">
+        {/* Most popular section */}
+        <section className="mt-[80px] mb-[200px] lg:w-full lg:mx-auto overflow-visible">
           <PopularProduct onChangeCollection={handleCollectionChange} isMouseOverMenu={isMouseOverMenu} />
         </section>
       </div>
