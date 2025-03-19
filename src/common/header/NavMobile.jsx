3fa8353fc656da5ad28e8e11bea439e43bee14e7
lse-7660 <lse76609251@gmail.com>
2025-03-19 15:32:21 +0900

    [feat, style] 헤더 반응형 및 애니메이션 추가

--- src/common/header/NavMobile.jsx
+++ src/common/header/NavMobile.jsx
@@ -2,12 +2,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { Icon } from '../../ui';
-import { useState } from 'react';
 import { navActions } from '../../store/modules/navSlice';
 
 const NavMobile = ({ isOpen, setIsOpen }) => {
   const { menuData, activeMenu } = useSelector((state) => state.nav);
   const dispatch = useDispatch();
+
+  const toggleTwoDepth = (x) => {
+    dispatch(navActions.setActiveMenu(x));
+    if (activeMenu === x) {
+      dispatch(navActions.clearMenu());
+    }
+  };
+
+  const closeAndResetActive = () => {
+    setIsOpen(false);
+    dispatch(navActions.clearMenu());
+  };
 
   const depth1Style = 'text-heading1-m font-diptyque';
   return (
@@ -15,43 +26,57 @@ const NavMobile = ({ isOpen, setIsOpen }) => {
       initial={{ x: '-100%', transformOrigin: 'left' }}
       animate={{ x: isOpen ? 0 : '-100%' }}
       transition={{ duration: 0.5, ease: 'easeOut' }}
-      className="fixed inset-0 z-20 px-6 py-[282px] bg-white"
+      className="nav-window-mobile fixed inset-0 z-20 px-6 py-[282px] bg-white desktop:hidden overflow-y-auto scrollbar-hide"
     >
-      <div onClick={() => setIsOpen(false)} className="cursor-pointer">
-        <Icon name="close" className="absolute top-0 right-0 m-6 r" />
+      <div onClick={closeAndResetActive} className="cursor-pointer">
+        <Icon name="close" className="absolute top-0 right-0 m-6 " />
       </div>
       <nav className={``}>
         {/* 1뎁스 메뉴 */}
         <ul className="flex flex-col gap-6">
           {menuData.map((menu) => (
             <li key={menu.id} className={depth1Style}>
-              <div
-                className={`flex flex-row items-center gap-6 ${
-                  activeMenu === menu.id ? 'border-b border-darkgrey-3' : ''
-                }`}
-              >
-                <Link to={menu.url}>{menu.menuName}</Link>
+              <div className={`relative flex flex-row items-center gap-6 w-fit transition-all ease-in-out`}>
+                <Link to={menu.url} onClick={closeAndResetActive}>
+                  {menu.menuName}
+                </Link>
                 {/* twodepth가 있는 경우 아이콘 추가 */}
                 {menu.twodepth && (
-                  <div onClick={() => dispatch(navActions.setActiveMenu(menu.id))}>
-                    <Icon name="keyboard_arrow_down" className="cursor-pointer" />
+                  <div onClick={() => toggleTwoDepth(menu.id)}>
+                    <Icon
+                      name={activeMenu === menu.id ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}
+                      className="cursor-pointer"
+                    />
                   </div>
                 )}
+                {/* 밑줄 애니메이션 */}
+                <span
+                  className={`absolute left-0 bottom-0 h-[1px] bg-darkgrey-3 transition-all duration-300 ease-in-out ${
+                    activeMenu === menu.id ? 'w-full' : 'w-0'
+                  }`}
+                />
               </div>
               {/* activeMenu인 경우에만 twodepth 렌더링 */}
-              {activeMenu === menu.id &&
-                menu.twodepth?.map((depth) => (
-                  <div key={depth.depthName}>
-                    <h3 className="text-heading3 font-diptyque mb-[10px]">{depth.depthName}</h3>
-                    <ul>
-                      {depth.depthList?.map((item) => (
-                        <li key={item.depthItem} className="mb-[10px] text-body3">
-                          <Link to={`/product${item.url}`}>{item.depthItem}</Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ))}
+              <motion.div
+                initial={{ height: 0, transformOrigin: 'top' }}
+                animate={{ height: activeMenu === menu.id ? 'auto' : 0 }}
+                transition={{ duration: 0.5, ease: 'easeInOut' }}
+                className="overflow-hidden"
+              >
+                {activeMenu === menu.id &&
+                  menu.twodepth?.map((depth) => (
+                    <div key={depth.depthName} className="">
+                      <h3 className="text-heading2-m font-diptyque mb-10 mt-10">{depth.depthName}</h3>
+                      <ul>
+                        {depth.depthList?.map((item) => (
+                          <li key={item.depthItem} className="mb-5 text-body2-m" onClick={closeAndResetActive}>
+                            <Link to={`/product${item.url}`}>{item.depthItem}</Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
+              </motion.div>
             </li>
           ))}
         </ul>
