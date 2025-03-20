dcf665d620938d7168971b1bbe8271b5c679588a
lse-7660 <lse76609251@gmail.com>
2025-03-20 15:01:55 +0900

    [feat, style] 검색 필터 올팩토리, 노트, 키워드 추가 및 페이지 스타일 마진 수정

--- src/pages/searchResult/index.jsx
+++ src/pages/searchResult/index.jsx
@@ -11,12 +11,17 @@ const SearchResult = () => {
     setText(e.target.value);
   };
 
-  const filteredData = allProductData.filter((item) =>
-    item.name.replace(/\s+/g, '').toLowerCase().includes(text.replace(/\s+/g, '').toLowerCase())
-  );
+  const filteredData = allProductData.filter((item) => {
+    const notesText = Array.isArray(item.notes) ? item.notes.map((n) => n.note).join(' ') : '';
+    const keywordsText = Array.isArray(item.keyword) ? item.keyword.map((k) => k.note).join(' ') : '';
+
+    return [item.name, item.olfactory, notesText, keywordsText].some((field) =>
+      field?.toString().replace(/\s+/g, '').toLowerCase().includes(text.replace(/\s+/g, '').toLowerCase())
+    );
+  });
 
   return (
-    <div className="desktop:max-w-[1360px] w-full mx-auto desktop:px-[80px] tablet:px-6 mobile:px-4 overflow-hidden">
+    <div className="desktop:max-w-[1360px] w-full mx-auto desktop:px-[80px] tablet:px-6 mobile:px-4 desktop:mb-[300px] tablet:mb-[80px] mobile:mb-[60px] overflow-hidden">
       <SearchForm onChange={onChange} text={text} />
       <SearchList filteredData={filteredData} />
     </div>
