12070542eb8e98a49a7777f9a444a7e58e1e2c29
rabbit-onion <csr1000@naver.com>
2025-03-20 12:27:30 +0900

    [style] 제품상세페이지 notes섹션 스와이퍼로 변경

--- src/components/productDetail/NotesSection.jsx
+++ src/components/productDetail/NotesSection.jsx
@@ -1,19 +1,32 @@
 import { useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+import { FreeMode } from 'swiper/modules';
 
 const NotesSection = () => {
   const { matchingNotesData } = useSelector((state) => state.product);
 
   return (
-    <div className="w-full my-sec-gap-pc">
+    <div className="flex flex-col w-full my-sec-gap-pc ">
       <h2 className="detail-sec-title">Story of Our Blend</h2>
-      {/* notes 최대개수 5개 : pc는 고정크기, tab,mobile은 swiper로 처리 */}
-      <div className="flex gap-6 justify-center items-center">
+
+      <Swiper
+        slidesPerView={5}
+        spaceBetween={24}
+        freeMode={true}
+        modules={[FreeMode]}
+        className="mySwiper notes-swiper"
+      >
         {matchingNotesData.map((data) => (
-          <div key={data.noteId} className="w-[21.5625rem] h-[21.5625rem]">
-            <img src={data.img} alt={data.note} className="h-full object-cover" />
-          </div>
+          <>
+            <SwiperSlide key={data.noteId}>
+              <img src={data.img} alt={data.note} className="w-full h-full object-cover" />
+            </SwiperSlide>
+          </>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
 };
